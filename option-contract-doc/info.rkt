0f3761286733613e86e4d2126b76206719f29bc1
Vincent St-Amour <stamourv@racket-lang.org>
2015-09-14 14:28:13 -0500

    Move docs from unstable-doc.

---
+++ option-contract-doc/info.rkt
@@ -0,0 +1,9 @@
+#lang info
+
+(define collection 'multi)
+(define version "1.0")
+(define deps '(("base" #:version "6.2.900.17")
+               "option-contract-lib"))
+(define build-deps '("scribble-lib"
+                     "racket-doc"))
+(define pkg-authors '(chrdimo robby))
