8adae00acee86c0719bc266786e79949de8ed49a
Vincent St-Amour <stamourv@racket-lang.org>
2015-09-14 14:13:11 -0500

    Move tests from unstable-test.

---
+++ option-contract-test/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define collection 'multi)
+(define version "1.0")
+(define deps '(("base" #:version "6.2.900.17")))
+(define build-deps '("rackunit-lib"
+                     "option-contract-lib"))
+(define pkg-authors '(chrdimo robby))
