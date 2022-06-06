42d22d5d01b7717911af1bde4baaa570ae3df516
Sam Tobin-Hochstadt <samth@indiana.edu>
2022-06-06 16:20:51 -0400

    Add `license` field to info.rkt files.
    Related to racket/racket#3760.

--- option-contract-lib/info.rkt
+++ option-contract-lib/info.rkt
@@ -5,3 +5,5 @@
 (define deps '(("base" #:version "6.2.900.17")))
 (define pkg-authors '(chrdimo robby))
 (define pkg-desc "Experimental libraries for option contracts")
+(define license
+  '(Apache-2.0 OR MIT))
